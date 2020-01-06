b81badf1d4d3ba5a4156eec2b97dc04157ec685a
Ryan Davis <ryandavis.au@gmail.com>
2020-01-06 22:25:35 +1000

    add crude codegen for IsRegistered and RegisterPageTypes

---
+++ PrismZero/PrismZero/DryIocZero/IZeroContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DryIocZero;
+
+namespace PrismZero.DryIocZero
+{
+    // we include an interface for the container so that our project
+    // still builds even if the container hasn't been generated
+    public interface IZeroContainer : IRegistrator, IResolverContext
+    {
+        void RegisterPageTypes();
+        bool IsRegistered(Type type, object key = null);
+    }
+}
